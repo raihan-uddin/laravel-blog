0dfe9d9dbee340f34840b6464cf3a6964a928ff5
Raihan Uddin <blackedition.me@gmail.com>
2018-02-26 10:15:25 +0600

    template for admin post, category, tags

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,15 @@ Route::get('post', function () {
 Route::get('admin/home', function () {
 	return view('admin.home');
 });
+
+Route::get('admin/post', function () {
+	return view('admin.post.post');
+});
+
+Route::get('admin/tag', function () {
+	return view('admin.tag.tag');
+});
+
+Route::get('admin/category', function () {
+	return view('admin.category.category');
+});
